b8e9387f2b7f73f8521efb5887e087d831e9cb06
root <root@AlexPC2.localdomain>
2026-02-07 19:11:17 +0000

    finished sequence.c

--- sequence.c
+++ sequence.c
@@ -15,22 +15,26 @@ long *Generate_2p3q_Seq(int n /*num ints*/, int *seq_size /*num elements*/)
 
     if (n > 0) {
         ptr[0] = 1;
+        printf("%ld\n", ptr[0]);
     } else {return ptr;}
-    long two, three;
-    two = 2; three = 3;
+    long p, q, two_n, three_n;
+    p = 0; q = 0;
     for (int i = 1; i < *seq_size; i ++) {
-        if (two < three) {
-            ptr[i] = two;
-            two += 2;
-        } else if (two > three) {
-            ptr[i] = three;
-            three += 3;
+        two_n = ptr[p] * 2;
+        three_n = ptr[q] * 3;
+
+        //choose smallest
+        if (two_n < three_n) {
+            ptr[i] = two_n;
+            p ++;
+        } else if (two_n > three_n) {
+            ptr[i] = three_n;
+            q ++;
         } else {
-            *(ptr + i) = two;
-            two += 2;
-            three += 3;
+            ptr[i] = two_n;
+            p ++; q ++;
         }
-        printf("%ld\n", ptr[i]);
+        printf("p:%ld, q:%ld, %ld\n", p, q, ptr[i]);
     }
 
     //returns the address of an array of long ints
